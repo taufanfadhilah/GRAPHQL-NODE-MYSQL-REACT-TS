0275c7d9072a987536bf1bbb4ef639393b649889
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-10-21 17:10:59 +0700

    create server of GraphQL

---
+++ server/src/Schema/TypeDefs/Messages.ts
@@ -0,0 +1,18 @@
+import {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLBoolean,
+} from "graphql";
+
+export const MessageType = new GraphQLObjectType({
+  name: "Message",
+  fields: () => ({
+    successful: {
+      type: GraphQLBoolean,
+    },
+    message: {
+      type: GraphQLString,
+    },
+  }),
+});
